58525a97dd4f24914e64ef117e97a491fddb6591
31415wataru <63274202+31415wataru@users.noreply.github.com>
2025-11-06 16:00:18 +0900

    Update kouka_car.py

--- kouka_car.py
+++ kouka_car.py
@@ -1,85 +1,62 @@
-let A = 0
-let C = 0
-let D = 0
-let timer = 0
-let timer2 = 0
-function ライントレース() {
-    
-    if (A == 4 || A == 6) {
-        Rover.MotorRunDual(0, 150)
-    } else if (A == 1 || A == 3) {
-        Rover.MotorRunDual(150, 0)
-    } else if (A == 2) {
-        Rover.MotorRunDual(100, 100)
-    } else if (A == 0) {
-        Rover.MotorRunDual(150, 150)
-    } else if (A == 7) {
-        Rover.MotorRunDual(100, 100)
-    } else {
-        Rover.MotorRunDual(0, 0)
-    }
-    
-}
-
-function 障害物回避() {
-    let timer: number;
-    let C: number;
-    let D: number;
-    
-    //  黒線を見つけるまで旋回
-    while (true) {
-        A = Rover.LineTracking()
-        timer = input.runningTime()
-        if (A != 0) {
-            //  黒線を検出したら停止して戻る
-            Rover.MotorRunDual(150, 0)
+def ライントレース():
+    if A == 4 or A == 6:
+        Rover.motor_run_dual(0, 150)
+    elif A == 1 or A == 3:
+        Rover.motor_run_dual(150, 0)
+    elif A == 2:
+        Rover.motor_run_dual(100, 100)
+    elif A == 0:
+        Rover.motor_run_dual(150, 150)
+    elif A == 7:
+        Rover.motor_run_dual(100, 100)
+    else:
+        Rover.motor_run_dual(0, 0)
+def 障害物回避():
+    global A
+    # 黒線を見つけるまで旋回
+    while True:
+        A = Rover.line_tracking()
+        timer3 = input.running_time()
+        if A != 0:
+            # 黒線を検出したら停止して戻る
+            Rover.motor_run_dual(150, 0)
             basic.pause(400)
             return
-        }
-        
-        //  ---- ① 最初の1秒間前進 ----
-        while (input.runningTime() - timer < 1000) {
-            A = Rover.LineTracking()
-            //  床の色を監視
-            if (A != 0) {
-                Rover.MotorRunDual(150, 0)
+        # ---- ① 最初の1秒間前進 ----
+        while input.running_time() - timer3 < 1000:
+            A = Rover.line_tracking()
+            # 床の色を監視
+            if A != 0:
+                Rover.motor_run_dual(150, 0)
                 basic.pause(400)
                 return
-            } else {
-                Rover.MotorRunDual(170, 120)
-            }
-            
-        }
-        C = 0
-        D = 0
-        while (C == 0) {
-            if (Rover.Ultrasonic() < 30) {
-                Rover.MotorRunDual(0, 0)
+            else:
+                Rover.motor_run_dual(170, 120)
+        C2 = 0
+        D2 = 0
+        while C2 == 0:
+            if Rover.ultrasonic() < 30:
+                Rover.motor_run_dual(0, 0)
                 break
-            }
-            
-            Rover.MotorRunDual(-100, 100)
-        }
-        while (D == 0) {
-            if (Rover.Ultrasonic() > 30) {
-                Rover.MotorRunDual(0, 0)
+            Rover.motor_run_dual(-100, 100)
+        while D2 == 0:
+            if Rover.ultrasonic() > 30:
+                Rover.motor_run_dual(0, 0)
                 break
-            }
-            
-            Rover.MotorRunDual(100, -100)
-        }
-    }
-}
+            Rover.motor_run_dual(100, -100)
+A = 0
+timer2 = 0
+timer = 0
+D = 0
+C = 0
 
-basic.forever(function on_forever() {
-    
-    A = Rover.LineTracking()
-    if (Rover.Ultrasonic() < 15) {
-        Rover.MotorRunDual(200, -200)
+def on_forever():
+    global A
+    A = Rover.line_tracking()
+    if Rover.ultrasonic() < 15:
+        Rover.motor_run_dual(200, -200)
         basic.pause(300)
         障害物回避()
-    } else {
+    else:
         ライントレース()
-    }
-    
-})
+basic.forever(on_forever)
